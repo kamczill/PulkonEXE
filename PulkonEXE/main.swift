c838a51e62e33f9bf1a24d2ceb3c0022df8535b0
Kamil Chrobak <kam134@interia.eu>
2017-09-30 10:58:36 +0200

    ogarnij to <3

--- PulkonEXE/main.swift
+++ PulkonEXE/main.swift
@@ -3,13 +3,13 @@ import Foundation
 func isArraySorted(array: [Int],condition : (Int, Int)->Bool)-> Bool
 {
     for i in 0..<array.count-1 {
-        if array[i] > array[i+1] {
+        if condition(array[i+1],array[i]) {
             return false
         }
-       }
+    }
     return true
 }
-let random : (Int , Int)-> Bool = {(arg1 : Int,arg2 : Int) -> Bool in
+let up : (Int , Int)-> Bool = {(arg1 : Int,arg2 : Int) -> Bool in
    
     if  arg1 < arg2
     {
@@ -17,6 +17,33 @@ let random : (Int , Int)-> Bool = {(arg1 : Int,arg2 : Int) -> Bool in
     }
     return false
 }
-var zmienna : [Int] = [1,2,3]
-print (isArraySorted(array: zmienna, condition: random))
+let down : (Int , Int)-> Bool = {(arg1 : Int,arg2 : Int) -> Bool in
+    
+    if  arg1 > arg2
+    {
+        return true
+    }
+    return false
+}
+let five : (Int , Int)-> Bool = {(arg1 : Int,arg2 : Int) -> Bool in
+    
+    if  arg1 != 5 && arg2 != 5
+    {
+        return true
+    }
+    return false
+}
+
+var zmienna : [Int] = [1,2,3,4,5]
+var zmienna2 : [Int] = [5,4,3,2,1]
+var zmienna3 : [Int] = [5,5,5,5,5]
+print (isArraySorted(array: zmienna, condition: up))
+print (isArraySorted(array: zmienna2, condition: down))
+print (isArraySorted(array: zmienna3, condition: five))
+
+print (isArraySorted(array: zmienna3, condition: up))
+print (isArraySorted(array: zmienna, condition: down))
+print (isArraySorted(array: zmienna2, condition: five))
 */
+
+
