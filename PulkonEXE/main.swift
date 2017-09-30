1949ade0eddbaf20888ff422b037595e6bbb1a1a
Kamil Chrobak <kam134@interia.eu>
2017-09-30 09:08:02 +0200

    init

---
+++ PulkonEXE/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PulkonEXE
+//
+//  Created by Kamil Chrobak on 30.09.2017.
+//  Copyright © 2017 Kamil Chrobak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print ("PULKON")
